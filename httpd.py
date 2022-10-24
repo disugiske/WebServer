7026986cd50c7847a734dc896791d1d456a036ee
disugiske <disugiske@yandex.ru>
2022-10-24 14:09:52 +0300

    Add logger, make dict from head_result func

--- httpd.py
+++ httpd.py
@@ -1,10 +1,10 @@
 import argparse
 import locale
+import logging
 import os
-import select
-import signal
 import socket
 import re
+import sys
 from multiprocessing import Pool
 from urllib.parse import unquote
 from datetime import datetime
@@ -29,22 +29,18 @@ head200 = "200 OK"
 head404 = "404 NOT FOUND"
 head405 = "405 Method Not Allowed\r\nAllow: GET, HEAD"
 head400 = "400 Bad Request"
+head_result = {
+    200: head200,
+    404: head404,
+    400: head400,
+    405: head405
+}
 
-
-def head_result(code):
-    if code == 200:
-        return head200
-    if code == 404:
-        return head404
-    if code == 400:
-        return head400
-    if code == 405:
-        return head405
 
 
 def head(content_length, content_type, code):
     locale.setlocale(locale.LC_ALL, "")
-    header = f"HTTP/1.1 {head_result(code)}\r\nDate:{datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')}\r\nServer: {servername}\r\nContent-Length: {content_length}\r\nContent-Type: {content_type}\r\nConnection: {connection}\r\n\r\n"
+    header = f"HTTP/1.1 {head_result[code]}\r\nDate:{datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')}\r\nServer: {servername}\r\nContent-Length: {content_length}\r\nContent-Type: {content_type}\r\nConnection: {connection}\r\n\r\n"
     return header.encode()
 
 
@@ -60,21 +56,21 @@ def parse_headers(request):
 
 def open_page(arg):
     with open(arg, "rb") as page:
-        response = page.read()
+        page_response = page.read()
         filesize = os.path.getsize(arg)
-    return response, filesize
+    return page_response, filesize
 
 
 def validate_adr(address, args):
     try:
         if address == "/":
-            response, filesize = open_page("index.html")
-            return response, head(filesize, "text/html;", 200)
+            page_response, filesize = open_page("index.html")
+            return page_response, head(filesize, "text/html;", 200)
         address = unquote(address)
 
         if re.fullmatch(r".*/\w*/", address):
-            response, filesize = open_page(f"{args}{address}index.html")
-            return response, head(filesize, "text/html;", 200)
+            page_response, filesize = open_page(f"{args}{address}index.html")
+            return page_response, head(filesize, "text/html;", 200)
 
         if re.fullmatch(r".*/.*\.\S*", address):
             if address[-1:] == "/":
@@ -85,19 +81,19 @@ def validate_adr(address, args):
                 file_type = re.fullmatch(r"(?P<root>.*/.*)\.(?P<type>.*)", address)
 
             content_type = http_content_type[file_type[2]]
-            response, filesize = open_page(f"{args}/{file_type[1]}.{file_type[2]}")
-            return response, head(filesize, content_type, 200)
+            page_response, filesize = open_page(f"{args}/{file_type[1]}.{file_type[2]}")
+            return page_response, head(filesize, content_type, 200)
 
         if re.fullmatch(r".*/\S*", address):
-            response, filesize = open_page(f"{args}{address}/index.html")
-            return response, head(filesize, "text/html;", 200)
+            page_response, filesize = open_page(f"{args}{address}/index.html")
+            return page_response, head(filesize, "text/html;", 200)
 
     except FileNotFoundError:
-        response, filesize = open_page("404.html")
-        return response, head(filesize, "text/html;", 404)
+        page_response, filesize = open_page("404.html")
+        return page_response, head(filesize, "text/html;", 404)
     except KeyError:
-        response, filesize = open_page("400.html")
-        return response, head(filesize, "text/html;", 400)
+        page_response, filesize = open_page("400.html")
+        return page_response, head(filesize, "text/html;", 400)
 
 
 def main(args):
@@ -105,6 +101,7 @@ def main(args):
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.bind((HOST, PORT))
     s.listen(1)
+    logging.info('Start server')
     while True:
         data = b""
         while data[-4:] != b"\r\n\r\n":
@@ -113,17 +110,18 @@ def main(args):
                 conn.settimeout(5)
                 data = conn.recv(1024)
             except Exception as e:
-                print(e)
+                logging.exception("Error!", e)
                 conn.close()
         address, method = parse_headers(data.decode())
+        logging.info(f"{method} method on {address}")
         if (method == None or method == "POST"):
-            response, filesize = open_page("405.html")
-            conn.sendall(head(filesize, "text/html;", 405) + response)
+            page_response, filesize = open_page("405.html")
+            conn.sendall(head(filesize, "text/html;", 405) + page_response)
             conn.close()
             continue
-        response, heads = validate_adr(address, args)
+        page_response, heads = validate_adr(address, args)
         if method == "GET":
-            conn.sendall(heads + response)
+            conn.sendall(heads + page_response)
             conn.close()
         if method == "HEAD":
             conn.sendall(heads)
@@ -136,6 +134,9 @@ if __name__ == "__main__":
     parser.add_argument("--root", "-r", help="Укажите путь к файлу DOCUMENT_ROOT")
     parser.add_argument("--worker", "-w", help="Укажите количество worker-ов")
     args = parser.parse_args()
+    logging.basicConfig(filename="server.log", level=logging.INFO,
+                        format='[%(asctime)s] %(levelname).1s %(message)s', datefmt='%Y.%m.%d %H:%M:%S')
+    handler = logging.StreamHandler(stream=sys.stdout)
     main(args.root)
     p = Pool(
         int(args.worker),
